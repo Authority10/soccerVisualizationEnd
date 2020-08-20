6251355a83d67ab247788e34b2e41caf534f1ed5
Authority10 <695916996@qq.com>
2020-08-20 10:18:07 +0800

    添加teams

--- app/config.js
+++ app/config.js
@@ -1,5 +1,7 @@
 module.exports={
     //Urldb:'mongodb+srv://Pupil:13011220116@bsu-lyosd.mongodb.net/test?retryWrites=true&w=majority',
-    url : "mongodb://localhost:27017/Soccer",
+    //阮的电脑是大写的S
+    // url : "mongodb://localhost:27017/Soccer",
+    url : "mongodb://localhost:27017/soccer",
     secret:'BSU'
 }
