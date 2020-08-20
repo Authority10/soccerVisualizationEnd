25cd474da51f7bf7fc5d55df3f69a3aba740aa89
rlf <17826859743@163.com>
2020-08-20 17:47:37 +0800

    change config

--- app/config.js
+++ app/config.js
@@ -2,6 +2,6 @@ module.exports={
     //Urldb:'mongodb+srv://Pupil:13011220116@bsu-lyosd.mongodb.net/test?retryWrites=true&w=majority',
     //阮的电脑是大写的S
     // url : "mongodb://localhost:27017/Soccer",
-    url : "mongodb://localhost:27017/soccer",
+    url : "mongodb://127.0.0.1:27017/soccer",
     secret:'BSU'
 }
